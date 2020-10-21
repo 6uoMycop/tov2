1a9414057eab6ec8910be6de9bcc31515721665f
6uoMycop <Arkady1234@yandex.ru>
2020-10-22 01:00:06 +0300

    initial

---
+++ Hidden/Driver.c
@@ -0,0 +1,134 @@
+#include <fltKernel.h>
+#include <Ntddk.h>
+//#include "ExcludeList.h"
+
+#include "RegFilter.h"
+#include "FsFilter.h"
+//#include "PsMonitor.h"
+//#include "Device.h"
+#include "Driver.h"
+#include "Configs.h"
+#include "Helper.h"
+
+#define DRIVER_ALLOC_TAG 'nddH'
+
+PDRIVER_OBJECT g_driverObject = NULL;
+
+volatile LONG g_driverActive = FALSE;
+
+// =========================================================================================
+
+VOID EnableDisableDriver(BOOLEAN enabled)
+{
+	InterlockedExchange(&g_driverActive, (LONG)enabled);
+}
+
+BOOLEAN IsDriverEnabled()
+{
+	return (g_driverActive ? TRUE : FALSE);
+}
+
+// =========================================================================================
+
+ULONGLONG g_hiddenRegConfigId = 0;
+ULONGLONG g_hiddenDriverFileId = 0;
+
+//NTSTATUS InitializeStealthMode(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
+//{
+//	PLDR_DATA_TABLE_ENTRY LdrEntry;
+//	UNICODE_STRING normalized;
+//	NTSTATUS status;
+//
+//	if (!CfgGetStealthState())
+//		return STATUS_SUCCESS;
+//	
+//	LdrEntry = (PLDR_DATA_TABLE_ENTRY)DriverObject->DriverSection;
+//
+//	normalized.Length = 0;
+//	normalized.MaximumLength = LdrEntry->FullModuleName.Length + NORMALIZE_INCREAMENT;
+//	normalized.Buffer = (PWCH)ExAllocatePoolWithQuotaTag(PagedPool, normalized.MaximumLength, DRIVER_ALLOC_TAG);
+//	
+//	if (!normalized.Buffer)
+//	{
+//		_InfoPrint("Error, can't allocate buffer");
+//		return STATUS_MEMORY_NOT_ALLOCATED;
+//	}
+//
+//	status = NormalizeDevicePath(&LdrEntry->FullModuleName, &normalized);
+//	if (!NT_SUCCESS(status))
+//	{
+//		_InfoPrint("Error, path normalization failed with code:%08x, path:%wZ", status, &LdrEntry->FullModuleName);
+//		ExFreePoolWithTag(normalized.Buffer, DRIVER_ALLOC_TAG);
+//		return status;
+//	}
+//
+//	status = AddHiddenFile(&normalized, &g_hiddenDriverFileId);
+//	if (!NT_SUCCESS(status))
+//		_InfoPrint("Error, can't hide self registry key");
+//
+//	ExFreePoolWithTag(normalized.Buffer, DRIVER_ALLOC_TAG);
+//
+//	status = AddHiddenRegKey(RegistryPath, &g_hiddenRegConfigId);
+//	if (!NT_SUCCESS(status))
+//		_InfoPrint("Error, can't hide self registry key");
+//
+//	_InfoPrint("Stealth mode has been activated");
+//	return STATUS_SUCCESS;
+//}
+
+// =========================================================================================
+
+_Function_class_(DRIVER_UNLOAD)
+VOID DriverUnload(PDRIVER_OBJECT DriverObject)
+{
+	UNREFERENCED_PARAMETER(DriverObject);
+
+	//DestroyDevice();
+	DestroyRegistryFilter();
+	DestroyFSMiniFilter();
+	//DestroyPsMonitor();
+}
+
+_Function_class_(DRIVER_INITIALIZE)
+NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
+{
+	NTSTATUS status;
+
+	//UNREFERENCED_PARAMETER(RegistryPath);
+
+	EnableDisableDriver(TRUE);
+
+	status = InitializeConfigs(RegistryPath);
+	if (!NT_SUCCESS(status))
+		_InfoPrint("Error, can't initialize configs");
+
+	EnableDisableDriver(CfgGetDriverState());
+
+	//status = InitializePsMonitor(DriverObject);
+	//if (!NT_SUCCESS(status))
+	//	_InfoPrint("Error, object monitor haven't started");
+
+	///status = InitializeFSMiniFilter(DriverObject);
+	///if (!NT_SUCCESS(status))
+	///	_InfoPrint("Error, file-system mini-filter haven't started");
+
+	status = InitializeRegistryFilter(DriverObject);
+	if (!NT_SUCCESS(status))
+		_InfoPrint("Error, registry filter haven't started");
+
+	//status = InitializeDevice(DriverObject);
+	//if (!NT_SUCCESS(status))
+	//	_InfoPrint("Error, can't create device");
+
+	//status = InitializeStealthMode(DriverObject, RegistryPath);
+	//if (!NT_SUCCESS(status))
+	//	_InfoPrint("Error, can't activate stealth mode");
+
+	DestroyConfigs();
+
+	DriverObject->DriverUnload = DriverUnload;
+	g_driverObject = DriverObject;
+
+	return STATUS_SUCCESS;
+}
+
---
+++ Hidden/Helper.h
@@ -0,0 +1,97 @@
+#pragma once
+
+#include <Ntddk.h>
+
+typedef enum _SYSTEM_INFORMATION_CLASS {
+	SystemBasicInformation = 0,
+	SystemPerformanceInformation = 2,
+	SystemTimeOfDayInformation = 3,
+	SystemProcessInformation = 5,
+	SystemProcessorPerformanceInformation = 8,
+	SystemInterruptInformation = 23,
+	SystemExceptionInformation = 33,
+	SystemRegistryQuotaInformation = 37,
+	SystemLookasideInformation = 45,
+	SystemPolicyInformation = 134,
+} SYSTEM_INFORMATION_CLASS;
+
+typedef struct _SYSTEM_PROCESS_INFORMATION {
+	ULONG           NextEntryOffset;
+	ULONG           NumberOfThreads;
+	LARGE_INTEGER   Reserved[3];
+	LARGE_INTEGER   CreateTime;
+	LARGE_INTEGER   UserTime;
+	LARGE_INTEGER   KernelTime;
+	UNICODE_STRING  ImageName;
+	KPRIORITY       BasePriority;
+	HANDLE          ProcessId;
+	HANDLE          InheritedFromProcessId;
+	ULONG           HandleCount;
+	UCHAR           Reserved4[4];
+	PVOID           Reserved5[11];
+	SIZE_T          PeakPagefileUsage;
+	SIZE_T          PrivatePageCount;
+	LARGE_INTEGER   Reserved6[6];
+} SYSTEM_PROCESS_INFORMATION, *PSYSTEM_PROCESS_INFORMATION;
+
+typedef struct _LDR_DATA_TABLE_ENTRY {
+	LIST_ENTRY     LoadOrder;
+	LIST_ENTRY     MemoryOrder;
+	LIST_ENTRY     InitializationOrder;
+	PVOID          ModuleBaseAddress;
+	PVOID          EntryPoint;
+	ULONG          ModuleSize;
+	UNICODE_STRING FullModuleName;
+	UNICODE_STRING ModuleName;
+	ULONG          Flags;
+	USHORT         LoadCount;
+	USHORT         TlsIndex;
+	union {
+		LIST_ENTRY Hash;
+		struct {
+			PVOID SectionPointer;
+			ULONG CheckSum;
+		} s;
+	} u;
+	ULONG   TimeStamp;
+} LDR_DATA_TABLE_ENTRY, *PLDR_DATA_TABLE_ENTRY;
+
+NTSYSAPI NTSTATUS NTAPI ZwQuerySystemInformation(
+	_In_      SYSTEM_INFORMATION_CLASS SystemInformationClass,
+	_Inout_   PVOID                    SystemInformation,
+	_In_      ULONG                    SystemInformationLength,
+	_Out_opt_ PULONG                   ReturnLength
+);
+
+NTSYSAPI NTSTATUS NTAPI ZwQueryInformationProcess(
+	_In_      HANDLE                    ProcessHandle,
+	_In_      PROCESSINFOCLASS          ProcessInformationClass,
+	_Out_     PVOID                     ProcessInformation,
+	_In_      ULONG                     ProcessInformationLength,
+	_Out_opt_ PULONG                    ReturnLength
+);
+
+NTSTATUS QuerySystemInformation(SYSTEM_INFORMATION_CLASS Class, PVOID* InfoBuffer, PSIZE_T InfoSize);
+NTSTATUS QueryProcessInformation(PROCESSINFOCLASS Class, HANDLE ProcessId, PVOID* InfoBuffer, PSIZE_T InfoSize);
+VOID FreeInformation(PVOID Buffer);
+
+#define NORMALIZE_INCREAMENT (USHORT)0x200
+
+NTSTATUS NormalizeDevicePath(PCUNICODE_STRING Path, PUNICODE_STRING Normalized);
+
+//#define _LogMsg(lvl, lvlname, frmt, ...) DbgPrintEx(DPFLTR_IHVDRIVER_ID, lvl , "hidden!" __FUNCTION__ "[irql:%d,pid:%d][" lvlname "]: " frmt "\n", (ULONG)KeGetCurrentIrql(), (ULONG)PsGetCurrentProcessId(), __VA_ARGS__)
+//
+//#define _InfoPrint(frmt,   ...) _LogMsg(DPFLTR_ERROR_LEVEL,   "error",   frmt, __VA_ARGS__)
+//#define _InfoPrint(frmt, ...) _LogMsg(DPFLTR_WARNING_LEVEL, "warning", frmt, __VA_ARGS__)
+//#define _InfoPrint(frmt,   ...) _LogMsg(DPFLTR_TRACE_LEVEL,   "trace",   frmt, __VA_ARGS__)
+//#define _InfoPrint(frmt,    ...) _LogMsg(DPFLTR_INFO_LEVEL,    "info",    frmt, __VA_ARGS__)
+
+#define DRIVER_NAME             L"RegFltr"
+#define _InfoPrint(str, ...)        \
+    DbgPrintEx(DPFLTR_IHVDRIVER_ID, \
+               DPFLTR_ERROR_LEVEL,  \
+               "%S: "##str"\n",     \
+               DRIVER_NAME,         \
+               __VA_ARGS__)
+
+
